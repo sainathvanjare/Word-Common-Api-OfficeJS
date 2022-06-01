664db499c222484d52336d332115f693258c4cd3
sainathvanjare <47569376+sainathvanjare@users.noreply.github.com>
2022-06-01 09:32:10 +0530

    Update main.ts
    updted file

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,12 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-// declare const Office: any;
+declare const Office: any;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
 
-// Office.initialize = function () {
-//   const platform = platformBrowserDynamic();
-//   platform.bootstrapModule(AppModule);
-// };
+Office.initialize = function () {
+  const platform = platformBrowserDynamic();
+  platform.bootstrapModule(AppModule);
+};
